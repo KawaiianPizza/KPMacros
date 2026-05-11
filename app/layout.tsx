bb93a3265b2b2aebf5ed3fd0d551fbf275cfae78
KawaiianPizza <KawaiinPizza@gmail.com>
2026-05-11 06:52:40 -0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const dynamic = "error"
 
 export const metadata: Metadata = {
-  title: "KPMacros UI v26.3.2",
+  title: "KPMacros UI v26.5.11",
   description: "UI for managing macros",
   icons: "icon.ico"
 }
